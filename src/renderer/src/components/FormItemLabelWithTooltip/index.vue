a0953aecc8c512e22dc2ad7b7338b31dfc166eb5
imzoke <imzoke@gmail.com>
2025-12-11 11:34:52 +0800

    chore: FormItem Label with Tooltip

---
+++ src/renderer/src/components/FormItemLabelWithTooltip/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <n-flex align="center" :size="2">
+    <span>{{ label }}</span>
+    <n-tooltip>
+      <template #trigger>
+        <n-icon>
+          <InfoCircle />
+        </n-icon>
+      </template>
+      <span>{{ tooltip }}</span>
+    </n-tooltip>
+  </n-flex>
+</template>
+
+<script setup lang="ts">
+import { NFlex, NTooltip, NIcon } from 'naive-ui';
+import { InfoCircle } from '@vicons/tabler';
+
+const props = defineProps({
+  label: {
+    type: String,
+    required: true
+  },
+  tooltip: {
+    type: String,
+    required: true
+  }
+});
+</script>
